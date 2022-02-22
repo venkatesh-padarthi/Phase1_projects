1c820cf98b853a658ae26f8c0bbbe7f4c8a88401
venkatesh-padarthi <venkateshpadarthi.36@gmail.com>
2022-02-22 18:42:59 +0530

    added changes

---
+++ 1.1.java
@@ -0,0 +1,39 @@
+
+public class typecasting {
+
+	public static void main(String[] args) {
+
+
+		System.out.println("Implicit Type Casting");
+		
+		//implicit conversion
+		
+		char a='B';
+		System.out.println("Value of a: "+a);
+		
+		int b=a;
+		System.out.println("Value of b: "+b);
+		
+		float c=a;
+		System.out.println("Value of c: "+c);
+		
+		long d=a;
+		System.out.println("Value of d: "+d);
+		
+		double e=a;
+		System.out.println("Value of e: "+e);
+		
+				
+		System.out.println("\n");
+		
+		System.out.println("Explicit Type Casting");
+		
+		//explicit conversion
+		
+		double x=48.56;
+		int y=(int)x;
+		System.out.println("Value of x: "+x);
+		System.out.println("Value of y: "+y);		
+	}
+
+}
