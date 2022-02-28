169de75ecb2925268742f680142dbe1faa54b882
venkatesh-padarthi <venkateshpadarthi.36@gmail.com>
2022-02-28 20:59:07 +0530

    added changes

---
+++ practice assisted projects/3.5.2 ThrowsKeyword.java
@@ -0,0 +1,22 @@
+public class ThrowsKeyword
+    {
+        void Division() throws ArithmeticException
+        {
+            int a=45,b=0,rs;
+rs = a / b;
+            System.out.print("\n\tThe result is : " + rs);
+        }
+         public static void main(String[] args)
+        {
+        	 ThrowsKeyword T = new ThrowsKeyword();
+             try
+            {
+                T.Division();
+            }
+            catch(ArithmeticException Ex)
+            {
+                System.out.print("\n\tError : " + Ex.getMessage());
+            }
+            System.out.print("\n\tEnd of program.");
+        }
+    }
