937dde09c2e43a33781997c0f0c354f5ae1a3fd3
venkatesh-padarthi <venkateshpadarthi.36@gmail.com>
2022-03-02 18:02:08 +0530

    added changes

---
+++ practice assisted projects/4.9 Queue.java
@@ -0,0 +1,17 @@
+public class Queue 
+{
+public static void main(String[] args) 
+{
+        		QueueString locationsQueue = new LinkedList();
+locationsQueue.add("Kolkata");
+        		locationsQueue.add("Patna");
+        		locationsQueue.add("Delhi");
+        		locationsQueue.add("Gurgaon");
+        		locationsQueue.add("Noida");
+System.out.println("Queue is : " + locationsQueue);
+        		System.out.println("Head of Queue : " + locationsQueue.peek());
+        		locationsQueue.remove();
+        		System.out.println("After removing Head of Queue : " + locationsQueue);
+        		System.out.println("Size of Queue : " + locationsQueue.size());
+    	}
+}
